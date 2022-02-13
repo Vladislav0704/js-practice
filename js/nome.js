ea9e82d6ec985a6b915ddf11f6f37caa90582239
Vladislav <onlinediviner@gmail.com>
2022-02-13 20:53:32 +0200

    asf

--- js/nome.js
+++ js/nome.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+let balance = 10000;
+const payment = 2000;
+
+console.log(`Общая стоимость тоавара ${payment} кредитов. Проверяем доступное количество доступных кредитовна счету. `);
+
+if (payment <= balance) {
+    console.log(`oK`);
+
+    balance -= payment;
+
+    console.log(`На счету осталось ${balance} кредитов`)
+};
